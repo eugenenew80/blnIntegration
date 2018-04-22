bbaed58841c560f2b4f9c01a099312f96db34b66
Eugene B <eugenenew80@gmail.com>
2018-04-22 20:30:55 +0600

    -

--- src/main/java/bln/integration/repo/ConnectionConfigRepository.java
+++
@@ -1,9 +0,0 @@
-package bln.integration.repo;
-
-import bln.integration.entity.ConnectionConfig;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConnectionConfigRepository extends JpaRepository<ConnectionConfig, Long> {
-}
