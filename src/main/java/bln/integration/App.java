0652d644dcf148f081e5f3e8a2926bc23d8847ac
eugenenew80 <eugenenew80@gmail.com>
2018-06-03 19:33:57 +0600

    -

--- src/main/java/bln/integration/App.java
+++ src/main/java/bln/integration/App.java
@@ -1,64 +1,19 @@
 package bln.integration;
 
-import bln.integration.exp.emcos.schedule.AutoEmcosExp;
-import bln.integration.imp.emcos.schedule.AutoEmcosImp;
-import bln.integration.imp.emcos.schedule.ManualEmcosImp;
-import bln.integration.imp.oic.schedule.AutoOicImp;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-import org.springframework.scheduling.support.CronTrigger;
 
 @EntityScan(
     basePackageClasses = { App.class, Jsr310JpaConverters.class }
 )
 @EnableScheduling
 @SpringBootApplication
-public class App implements ApplicationListener<ApplicationReadyEvent> {
+public class App  {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
-    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
-        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
-        taskScheduler.setPoolSize(5);
-        taskScheduler.initialize();
-
-        taskScheduler.schedule(autoEmcosImp, new CronTrigger(autoEmcosImpCronExpr));
-        taskScheduler.schedule(manualEmcosImp, new CronTrigger(manualEmcosImpCronExpr));
-        taskScheduler.schedule(autoOicImp, new CronTrigger(autoOicImpCronExpr));
-        taskScheduler.schedule(autoEmcosExp, new CronTrigger(autoEmcosExpCronExpr));
-    }
-
-
-    @Value("${bln.integration.imp.emcos.schedule.autoEmcosImp.cron}")
-    private String autoEmcosImpCronExpr;
-
-    @Value("${bln.integration.imp.emcos.schedule.manualEmcosImp.cron}")
-    private String manualEmcosImpCronExpr;
-
-    @Value("${bln.integration.imp.oic.schedule.autoOicImp.cron}")
-    private String autoOicImpCronExpr;
-
-    @Value("${bln.integration.exp.emcos.schedule.autoEmcosExp.cron}")
-    private String autoEmcosExpCronExpr;
-
-    @Autowired
-    private AutoEmcosImp autoEmcosImp;
-
-    @Autowired
-    private ManualEmcosImp manualEmcosImp;
-
-    @Autowired
-    private AutoOicImp autoOicImp;
-
-    @Autowired
-    private AutoEmcosExp autoEmcosExp;
 }
--- src/main/java/bln/integration/AppConfig.java
+++ src/main/java/bln/integration/AppConfig.java
@@ -1,15 +1,25 @@
 package bln.integration;
 
+import bln.integration.exp.emcos.schedule.AutoEmcosExp;
+import bln.integration.imp.emcos.schedule.AutoEmcosImp;
+import bln.integration.imp.emcos.schedule.ManualEmcosImp;
+import bln.integration.imp.oic.schedule.AutoOicImp;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.support.CronTrigger;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements ApplicationListener<ApplicationReadyEvent> {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
@@ -19,4 +29,40 @@ public class AppConfig {
         mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         return mapper;
     }
+
+
+    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(5);
+        taskScheduler.initialize();
+
+        taskScheduler.schedule(autoEmcosImp, new CronTrigger(autoEmcosImpCronExpr));
+        taskScheduler.schedule(manualEmcosImp, new CronTrigger(manualEmcosImpCronExpr));
+        taskScheduler.schedule(autoOicImp, new CronTrigger(autoOicImpCronExpr));
+        taskScheduler.schedule(autoEmcosExp, new CronTrigger(autoEmcosExpCronExpr));
+    }
+
+    @Value("${bln.integration.imp.emcos.schedule.autoEmcosImp.cron}")
+    private String autoEmcosImpCronExpr;
+
+    @Value("${bln.integration.imp.emcos.schedule.manualEmcosImp.cron}")
+    private String manualEmcosImpCronExpr;
+
+    @Value("${bln.integration.imp.oic.schedule.autoOicImp.cron}")
+    private String autoOicImpCronExpr;
+
+    @Value("${bln.integration.exp.emcos.schedule.autoEmcosExp.cron}")
+    private String autoEmcosExpCronExpr;
+
+    @Autowired
+    private AutoEmcosImp autoEmcosImp;
+
+    @Autowired
+    private ManualEmcosImp manualEmcosImp;
+
+    @Autowired
+    private AutoOicImp autoOicImp;
+
+    @Autowired
+    private AutoEmcosExp autoEmcosExp;
 }
